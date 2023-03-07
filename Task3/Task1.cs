8f4addaf83be94c30eb017533f5c3ce486348197
RayzOne <99681039+RayzOnePy@users.noreply.github.com>
2023-03-07 10:14:37 +0700

    Add files via upload

---
+++ Task3/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+public class Task1
+{
+    public class Worker
+    {
+        private string name;
+        public string Name {get{return name;}}
+        
+        private string surname;
+        public string Surname {get{return surname;}}
+        
+        private int rate;
+        public int Rate {get{return rate;}}
+        
+        private int days;
+        public int Days {get{return days;}}
+
+        public int GetSalary()
+        {
+            return rate * days;
+        }
+
+        public Worker(string name, string surname, int rate, int days)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.rate = rate;
+            this.days = days;
+        }
+    }
+    static void Main()
+    {
+        Worker worker = new Worker("Dima", "Vasiliev", 5000,365);
+        Console.WriteLine(worker.GetSalary());
+    }
+}
---
+++ Task3/Task3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+public class Task3
+{
+    public class Calculation
+    {
+        private string calculationLine;
+
+        public void SetCalculationLine(string line)
+        {
+            calculationLine = line;
+        }
+
+        public string GetCalculationLine()
+        {
+            return calculationLine;
+        }
+
+        public void SetLastSymbolCalculationLine(char letter)
+        {
+            // calculationLine = calculationLine + letter; так проще
+            calculationLine = calculationLine.Insert(calculationLine.Length, letter.ToString());
+        }
+
+        public char GetLastSymbol()
+        {
+            return calculationLine[calculationLine.Length - 1];
+        }
+
+        public void DeleteLastSymbol()
+        {
+            calculationLine = calculationLine.Remove(calculationLine.Length - 1);
+        }
+    }
+    static void Main()
+    {
+        Calculation calculation = new Calculation();
+        calculation.SetCalculationLine("Vasiliev");
+        Console.WriteLine(calculation.GetCalculationLine());
+        calculation.SetLastSymbolCalculationLine('D');
+        Console.WriteLine(calculation.GetCalculationLine());
+        Console.WriteLine(calculation.GetLastSymbol());
+        calculation.DeleteLastSymbol();
+        Console.WriteLine(calculation.GetCalculationLine());
+    }
+}
